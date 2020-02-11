d56259832894bbc7c84b49c43c09818631e2427b
Felix Rodriguez <frod25@gmail.com>
2020-02-11 18:32:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,12 @@
 def square_array(array)
   index = 0
-  puts "#{index}"
-  puts "#{array}"
+  puts "index before iteration #{index}"
+  puts "array before iteration #{array}"
   new_array = []
   array.each do |item|
     new_array[index] = item ** 2
     index++
+    puts "updated index #{index}"
   end
 end
 test_array = [1,2,3]
